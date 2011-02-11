8560e39fe480ff2cb6cb0eacc573d1f8e5aabc5f
Tom Bevers <t.bevers@gmail.com>
2011-02-11 01:24:19 +0100

    Added dashboard controller and forwarded index controller to it

--- application/controllers/DashboardController.php
+++ application/controllers/DashboardController.php
@@ -1,6 +1,6 @@
 <?php
 
-class IndexController extends Zend_Controller_Action
+class DashboardController extends Zend_Controller_Action
 {
 
     public function init()
@@ -10,7 +10,7 @@ class IndexController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        $this->_forward('index', 'dashboard');
+        // action body
     }
 
 
