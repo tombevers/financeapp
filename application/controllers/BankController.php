50dfd99655fcce92a69ff7c106cab040f4dea451
Tom Bevers <t.bevers@gmail.com>
2011-02-12 07:14:03 +0100

    Created the BankController

---
+++ application/controllers/BankController.php
@@ -0,0 +1,44 @@
+<?php
+
+class BankController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        $this->_forward('list');
+    }
+
+    public function listAction()
+    {
+        // action body
+    }
+
+    public function createAction()
+    {
+        // action body
+    }
+
+    public function editAction()
+    {
+        // action body
+    }
+
+    public function deleteAction()
+    {
+        // action body
+    }
+}
+
+
+
+
+
+
+
+
+
