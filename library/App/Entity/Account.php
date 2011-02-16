9c180eb3e40231626f978a5d62dee05bb6b5c847
Tom Bevers <t.bevers@gmail.com>
2011-02-16 18:50:08 +0100

    Added AccountRepository

--- library/App/Entity/Account.php
+++ library/App/Entity/Account.php
@@ -4,7 +4,7 @@ namespace App\Entity;
 
 /**
  * @Table(name="accounts")
- * @Entity
+ * @Entity(repositoryClass="App\Entity\Repository\AccountRepository")
  */
 class Account
 {
---
+++ library/App/Entity/Repository/AccountRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Entity\Repository;
+
+use Doctrine\ORM\EntityRepository,
+    App\Entity\Account;
+
+class AccountRepository extends EntityRepository
+{
+    
+}
