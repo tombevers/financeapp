b99ba808b2e14bd96eaba998334c1c40332a52ad
Tom Bevers <t.bevers@gmail.com>
2011-03-06 09:55:13 +0100

    Added save and remove function to the payee repository

--- library/App/Entity/Repository/PayeeRepository.php
+++ library/App/Entity/Repository/PayeeRepository.php
@@ -7,5 +7,32 @@ use Doctrine\ORM\EntityRepository,
 
 class PayeeRepository extends EntityRepository
 {
+    /**
+     * Saves a payee
+     * 
+     * @param Payee $payee
+     * @param array $values 
+     */
+    public function savePayee(Payee $payee, array $values)
+    {
+        $bank->setName($values['name']);
+        $bank->setAddress($values['address']);
+        $bank->setPhone($values['phone']);
+        $bank->setEmail($values['email']);
+
+        $this->getEntityManager()->persist($bank);
+    }
     
+    /**
+     * Removes a payee
+     * 
+     * @param int $payeeId 
+     */
+    public function removePayee($payeeId)
+    {
+        $em = $this->getEntityManager();
+        $proxy = $em->getReference('\App\Entity\Payee', $payeeId);
+
+        $em->remove($proxy);
+    }
 }
