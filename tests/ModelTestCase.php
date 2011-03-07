fc13e8a864514b8d574afc5fcf8e520e90abf62a
Tom Bevers <t.bevers@gmail.com>
2011-03-07 20:02:48 +0100

    Simplified ModelTestCase

--- tests/ModelTestCase.php
+++ tests/ModelTestCase.php
@@ -18,37 +18,16 @@ class ModelTestCase extends PHPUnit_Framework_TestCase
         $application->bootstrap();
         $this->_doctrineContainer = Zend_Registry::get('doctrine');
         $this->_em = $this->_doctrineContainer->getEntityManager();
-        self::_dropSchema($this->_doctrineContainer->getConnection()->getParams());
 
-        $tool = new \Doctrine\ORM\Tools\SchemaTool($this->_doctrineContainer->getEntityManager());
-        $metas = $this->_getClassMetas(APPLICATION_PATH . '/../library/App/Entity','App\Entity\\');
-        $tool->createSchema($metas);
+        $tool = new \Doctrine\ORM\Tools\SchemaTool($this->_em);
+        $tool->dropDatabase();
+        $tool->createSchema($this->_em->getMetadataFactory()->getAllMetadata());
     }
 
     protected function tearDown()
     {
-        self::_dropSchema($this->_doctrineContainer->getConnection()->getParams());
-    }
-
-    private static function _dropSchema($params)
-    {
-        if (file_exists($params['path'])) {
-            unlink ($params['path']);
-        }
-    }
-
-    private function _getClassMetas($path, $namespace)
-    {
-        $metas = array();
-        $handle = opendir($path);
-        if ($handle) {
-            while(false !== ($file = readdir($handle))) {
-                if(strstr($file,'.php')) {
-                    list($class) = explode('.',$file);
-                    $metas[] = $this->_doctrineContainer->getEntityManager()->getClassMetadata($namespace . $class);
-                }
-            }
-        }
-        return $metas;
+        $this->_doctrineContainer->getConnection()->close();
+        $tool = new \Doctrine\ORM\Tools\SchemaTool($this->_em);
+        $tool->dropDatabase();
     }
 }
