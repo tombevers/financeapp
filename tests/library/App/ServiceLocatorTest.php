5e1037e28ae125d8ea1543f187cbeaf8e110d4a4
Tom Bevers <t.bevers@gmail.com>
2011-09-28 06:39:04 +0200

    ServiceLocator unit test

---
+++ tests/library/App/ServiceLocatorTest.php
@@ -0,0 +1,46 @@
+<?php
+
+class ServiceLocatorTest extends PHPUnit_Framework_TestCase
+{
+    
+    /**
+     * @dataProvider serviceProvider
+     */
+    public function testGeters($expected, $getter)
+    {
+        $serviceLocator = new \App\ServiceLocator();
+        
+        $this->assertInstanceOf(
+            $expected,
+            $serviceLocator->$getter()
+        );
+    }
+
+    public function serviceProvider()
+    {
+        return array(
+            array(
+                '\Application_Service_Bank',
+                'getBankService'
+            ),
+            array(
+                '\Application_Service_Account',
+                'getAccountService'
+            ),
+            array(
+                '\Application_Service_AccountType',
+                'getAccountTypeService'),
+            array(
+                '\Application_Service_Payee',
+                'getPayeeService'),
+            array(
+                '\Application_Service_Transaction',
+                'getTransactionService'),
+            array(
+                '\Application_Service_TransactionType',
+                'getTransactionTypeService'
+            ),
+        );
+    }
+    
+}
